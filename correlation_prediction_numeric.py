515ad38a8799c990d5f0f5ff0aee3352ec3fb742
rolansy <ronalshoeygeorge@gmail.com>
2024-08-19 13:57:46 +0530

    Refactor correlation prediction code for numeric attributes

--- correlation_prediction_numeric.py
+++ correlation_prediction_numeric.py
@@ -3,15 +3,19 @@ df=pd.read_csv('corr_numeric.csv')
 am=df.a.mean()
 bm=df.b.mean()
 n=len(df)
-print('Correlation Coefficient: ',round(sum([(df.a[i]-am)*(df.b[i]-bm) for i in range(len(df))])/(len(df)*df.a.std()*df.b.std()),2))
 s=0
 for i in range(n):
-    s+=(df.a[i]-am)*(df.b[i])
+    s+=(df.a[i]-am)*(df.b[i]-bm)
 asd=df.a.std()
 bsd=df.b.std()
-print('Covariance Coefficient: ',s/(n*asd*bsd))
+corr=s/(n*asd*bsd)
+print('Correlation Coefficient: ',corr)
+print('Covariance : ',s/(n))
+if corr>0:
+    print('Positive Correlation')
+elif corr<0:
+    print('Negative Correlation')
+else:
+    print('No Correlation')
 
-print('COvariance : ')
-print(df.cov())
-print(s/(n-1))
 
